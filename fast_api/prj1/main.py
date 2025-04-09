bd87c89d438e67e1ec34d8168dbc9bfb418cc25f
LUser8 <atultiwarikec@gmail.com>
2025-04-09 15:05:42 +0530

    Updated fastapi learing

---
+++ fast_api/prj1/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, HTTPException
+from schema import GenreURLChoice, Band
+
+app = FastAPI()
+    
+BANDS = [
+    {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
+    {'id': 2, 'name': 'Apex Twin', 'genre': 'Electronic'},
+    {'id': 3, 'name': 'The Slowdive', 'genre': 'metal', 'albums':[
+        {'title': 'First Album', 'release_date': '2025-01-12'}
+    ]},
+    {'id': 4, 'name': 'Wu-Tang clan', 'genre': 'Hip-Hop'}
+]
+
+@app.get('/bands')
+async def bands(
+    genre: GenreURLChoice | None=None,
+    has_album: bool = False
+    ) -> list[Band]:
+    print(has_album)
+    band_list = [Band(**band) for band in BANDS]
+    if genre:
+        band_list = [band for band in band_list if band.genre.lower() == genre.value]
+    if has_album:
+        band_list = [band for band in band_list if len(band.albums)>0]
+    return band_list
+
+@app.get('/bands/{band_id}')
+async def band(band_id: int) -> Band:
+    band = next((Band(**band) for band in BANDS if band['id'] == band_id), None)
+    if band is None:
+        raise HTTPException(status_code=404, detail='Band not found')
+    return band
+
+@app.get('/bands/genre/{genre}')
+async def bands_for_genre(genre: GenreURLChoice) -> list[band]:
+    return [Band(**band) for band in BANDS if band['genre'].lower() == genre.value]
